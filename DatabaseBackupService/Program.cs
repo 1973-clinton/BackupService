2facd7f1cbf9eb31e59dc597c5c38c492ef1aeac
1973-clinton <clintonboamahofficial@gmail.com>
2022-01-19 16:49:54 +0000

    Completed Serilog setup and configuration

--- DatabaseBackupService/Program.cs
+++ DatabaseBackupService/Program.cs
@@ -3,19 +3,52 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
 
 namespace DatabaseBackupService
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.File(new CompactJsonFormatter(), @"D:\Services\BackupService\logs\logs.json")
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "Host terminated unexpectedly");
+                return 1;
+            }
+
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+            //CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.File(@"D:\Services\BackupService\logs\logs.txt"))
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddLogging();
